b6b34d504c9a39b72e3adda2921e914e81cd980b
C4be <dmitryc4be@gmail.com>
2026-05-30 18:35:55 +0300

    sec

--- load_cert.sh
+++ load_cert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir -p ~/certbot/conf/live/wedding-site
-cat cert/certificate.crt cert/certificate_ca.crt > ~/certbot/conf/live/wedding-site/fullchain.pem
-cp cert/certificate.key ~/certbot/conf/live/wedding-site/privkey.pem
+mkdir -p ./certbot/conf/live/wedding-site
+cat cert/certificate.crt cert/certificate_ca.crt > ./certbot/conf/live/wedding-site/fullchain.pem
+cp cert/certificate.key ./certbot/conf/live/wedding-site/privkey.pem
